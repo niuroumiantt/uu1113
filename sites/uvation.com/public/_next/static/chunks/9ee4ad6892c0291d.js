b4ed1dfc108e13e7ef1595051a12af83c8d37190
niuroumiantt <niuroumiantt@users.noreply.github.com>
2025-11-14 08:14:35 +0800

    Add missing Next.js chunks to sites copy

---
+++ sites/uvation.com/public/_next/static/chunks/9ee4ad6892c0291d.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,52498,t=>{t.v(e=>Promise.all(["static/chunks/9b8afd9b1949efdf.js"].map(e=>t.l(e))).then(()=>e(171725)))}]);
